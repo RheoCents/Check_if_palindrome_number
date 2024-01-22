4a396f659d0f73c9d5434eb843407a8fb5f9206b
unknown <centinaraphael0157@gmail.com>
2024-01-23 01:03:51 +0800

    code test

--- Check_if_palindrome_number.py
+++ Check_if_palindrome_number.py
@@ -1,7 +1,16 @@
 #Write a program to check if the given number is a palindrome number.
 
-#A palindrome number is a number that is the same after reverse. For example, 545, is the palindrome numbers
+#A palindrome number is a number that is the same after reverse. 
+#For example, 545, is the palindrome numbers
 
-user_input_number = int(input('Good day, please give me a number '))
+user_input_number = input('Good day, please give me a number ')
 number_of_places_in_the_number = len(user_input_number)
 print (number_of_places_in_the_number)
+negative_number_of_places_in_the_number = int(number_of_places_in_the_number) - int(number_of_places_in_the_number)*2 
+print (negative_number_of_places_in_the_number)
+
+for i in range (-1, negative_number_of_places_in_the_number, -1):
+    if user_input_number == user_input_number[i]:
+        print('The inputed number is a Palindrome number')
+    else:
+        print('The inputed number is a Palindrome number')
