7d3d40d0c63c4d65fd69938acfa728f2049ebb38
unknown <centinaraphael0157@gmail.com>
2024-01-23 01:25:07 +0800

    code worked (wrong result is caused by wrong print)

--- Check_if_palindrome_number.py
+++ Check_if_palindrome_number.py
@@ -4,11 +4,9 @@
 #For example, 545, is the palindrome numbers
 
 user_input_number = input('Good day, please give me a number ')
-number_of_places_in_the_number = len(user_input_number)
 reversed_user_input_number = int(str(user_input_number)[::-1]) 
-print (number_of_places_in_the_number)
 quotient_of_original_and_reversed = int(user_input_number)/int(reversed_user_input_number)
-if  quotient_of_original_and_reversed == 1:
+if  quotient_of_original_and_reversed == 1.0:
     print('The inputed number is a Palindrome number')
 else:
-    print('The inputed number is a Palindrome number')
+    print('The inputed number is a not Palindrome number')
